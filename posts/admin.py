018ee468a449534f543754416e24ea37d77e34b5
FreezingMoon <oyaroslav2004@gmail.com>
2021-07-22 07:29:32 +0300

    Edited admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
-=======
+
 from django.contrib import admin
 
 # Register your models here.
->>>>>>> a40d8a2 (Initial commit)
